ba697268dc70735f1d19065b624ca978081da962
Kevin <kevin.qi@berkeley.edu>
2020-01-06 10:16:20 -0800

    new react app

---
+++ src/scenes/Game.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Loop, Stage } from 'react-game-kit';
+
+function Game() {
+  return (
+    <Loop>
+    	<Stage>
+    		<World>
+    		</World>
+    	</Stage>
+    </Loop>
+  );
+}
+
+export default Game;
